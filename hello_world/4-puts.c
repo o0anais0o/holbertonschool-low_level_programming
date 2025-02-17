d3d22ad4df9762b8389fa7a904bd30d985115b1c
o0anais0o <choisy_anais@hotmail.fr>
2025-02-17 15:46:46 +0100

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,2 @@
 #!/bin/bash
-#include <stdio.h>
-
-int main(void) { puts("Programming is like building a multilingual puzzle"); return 0; }
+puts( "\"Programming is like building a multilingual puzzle.0" );
