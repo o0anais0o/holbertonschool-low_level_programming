7bd712d6007836bc87c61e7f831c0ea2c08d7044
o0anais0o <choisy_anais@hotmail.fr>
2025-02-18 16:01:28 +0100

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int numbers;
+
+	for (nombers = 0; nombers < 10; nombers++)
+	{
+
+		putchar(nombers + '0');
+		if (nombers < 9);
+	}
+	putchar('\n');
+	return (0);
+}
