b7c98e7c7ad786861f067e1b087ebd386c535922
o0anais0o <choisy_anais@hotmail.fr>
2025-02-18 15:10:11 +0100

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -12,7 +12,7 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
-		putchar(alphabetminuscule);
+		putchar(alphabet);
 	
 	for (alphabet = 'A'; alphabet <= 'Z' ; alphabet++)
  		putchar(alphabet);
