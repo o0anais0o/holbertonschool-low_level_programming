1a69a81517070764c67fb265a2584caada35076a
o0anais0o <choisy_anais@hotmail.fr>
2025-02-19 11:28:13 +0100

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include "main.h"
+#include <main.h>
 
 /**
  * main - affiche _putchar suivi d'une nouvelle ligne
  *
  * Return: Toujours 0 (Succès)
  */
-int main(void) 
+
+int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 
 	return (0);
+}
