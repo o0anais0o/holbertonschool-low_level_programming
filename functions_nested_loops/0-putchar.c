9326931c033ba3bc9fff27d4dc725ed799fedda9
o0anais0o <choisy_anais@hotmail.fr>
2025-02-19 11:03:56 +0100

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * main - affiche _putchar suivi d'une nouvelle ligne
+ *
+ * Return: Toujours 0 (Succès)
+ */
+int main(void) 
+{
+
+    putchar('\n');
+    return 0;
+}
+
