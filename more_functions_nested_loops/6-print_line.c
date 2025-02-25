e3664eace8faac325f9fef6537b79b9423a63bc3
o0anais0o <choisy_anais@hotmail.fr>
2025-02-25 13:55:40 +0100

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -5,6 +5,7 @@
  * Where n is the number of times the character _ should be printed
  * The line should end with a \n
  * If n is 0 or less, the function should only print \n
+ * @n: number of times the character _ should be printed
  * Return: nothing
  */
 void print_line(int n)
