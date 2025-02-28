d56891f543f4889de05ba1710935968bb1334b1c
o0anais0o <choisy_anais@hotmail.fr>
2025-02-28 11:26:03 +0100

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - Met en majuscule la première lettre de chaque mot
+ * @str: La chaîne à modifier
+ *
+ * Return: Un pointeur vers la chaîne modifiée
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	int capitalize_next = 1;
+
+	while (str[i] != '\0')
+	{
+		if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+
+		capitalize_next = 0;
+
+		switch (str[i])
+		{
+			case ' ': case '\t': case '\n': case ',': case ';':
+			case '.': case '!': case '?': case '"': case '(':
+			case ')': case '{': case '}':
+				capitalize_next = 1;
+				break;
+		}
+
+		i++;
+	}
+
+	return (str);
+}
