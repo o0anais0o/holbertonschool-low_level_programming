0191431ca82e62568ba2ac4dadb5c8ac1a7c199e
o0anais0o <choisy_anais@hotmail.fr>
2025-03-03 17:16:57 +0100

    0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * memset - Remplit les n premiers octets de la zone mémoire
+ * 	pointée par @s avec l'octet constant @c
+ * @s : un pointeur vers la zone mémoire à remplir.
+ * @c : le caractère avec lequel remplir la zone mémoire.
+ * @n : le nombre d'octets à remplir.
+ *
+ * Return : un pointeur vers la zone mémoire remplie @s.
+ */
+void *_memset(void *s, int c, size_t n)
+{
+	unsigned int index;
+	unsigned char *memory = s, value = c;
+
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+
+	return (memory);
+}
