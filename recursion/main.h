e8c20065c419837e6034c0447ea1a0e1d60efa5b
o0anais0o <choisy_anais@hotmail.fr>
2025-03-05 16:20:43 +0100

    0-puts_recursion.c

--- recursion/main.h
+++ recursion/main.h
@@ -1,5 +1,3 @@
-#include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -7,7 +5,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
