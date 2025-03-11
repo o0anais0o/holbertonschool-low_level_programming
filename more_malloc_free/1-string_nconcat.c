dbd5e29a93562f8724c72171ca4f406730004377
o0anais0o <choisy_anais@hotmail.fr>
2025-03-11 14:02:44 +0100

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -3,14 +3,19 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenates two strings using at..
- * ..most an inputted number of bytes.
- * @s1: the firts strings.
- * @s2: the second strings.
- * @n: the maximum number of bytes of s2 to concatenated to s1.
- * return: if the function fails - NULL.
- *          otherwise - a pointer to the concatenated space in memory.
- */
+* string_nconcat - concatenates two strings using at..
+* ..most an inputted number of bytes.
+* @s1: the firts strings.
+* @s2: the second strings.
+* @n: the maximum number of bytes of s2 to concatenated to s1.
+*
+* Return: if the function fails - NULL.
+*         otherwise - a pointer to the concatenated space in memory.
+*
+* Description: If s1 is NULL, it is treated as an empty string.
+* If s2 is NULL, it is treated as an empty string.
+* If n is greater than or equal to the length of s2, use the entire s2.
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *concat;
