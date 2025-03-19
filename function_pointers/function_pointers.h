b473486d58ab1d07de83048b4bd75e7cc8d281ec
o0anais0o <choisy_anais@hotmail.fr>
2025-03-19 15:35:10 +0100

    Update function_pointers.h: clean up header file by removing comments and unnecessary includes

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,16 +1,10 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
 
-/*
- * File: function_pointers.h
- * Desc: Header file containing prototypes for all functions
- *       used in the function_pointers directory.
- */
-
-#include <stdlib.h>
-
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+#endif /* FUNCTION_POINTERS_H */
