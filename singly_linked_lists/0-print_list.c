6c500652cf5ec0051f212e84adb802366742a3fa
o0anais0o <choisy_anais@hotmail.fr>
2025-04-01 12:59:25 +0200

    Modificate 0-print_list.c file

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,25 +12,17 @@ size_t print_list(const list_t *h)
 {
 size_t node_count = 0;
 const list_t *current = h;
-current->len
-current->str
 
-if (h == NULL)
-return (0);
-
-{
 while (current != NULL)
-current = current->next;
+{
+if (current->str == NULL)
+printf("[0] (nil)\n)");
+else
+printf("[%d] %s\n", current->len, current->str);
+
 node_count++;
+current = current->next;
 }
 
-if (current->str == NULL)
-{
-printf("[0] (nil)\n)");
-}
-else
-{
-printf("[%d] %s\n", current->len, curent->str);
-}
 return (node_count);
 }
