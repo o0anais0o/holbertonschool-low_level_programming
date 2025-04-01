31d853ba86ec962d4f255b1c08152739defefb2b
o0anais0o <choisy_anais@hotmail.fr>
2025-04-01 10:43:53 +0200

    Create a _putcar.c file and 0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
+ */
+
+size_t print_list(const list_t *h)
+{
+int count_byte = 0;
+int index;
+node = "[len] str";
+
+for (index = 0; node[index] != "NULL"; index++)
+
+if (h == NULL)
+Return('0');
+
+
+printf("[5] "Hello"\n [5] "World"");
+printf("\n");
+printf("[0] (nil)\n [5] "World"");
+count_byte++
+
+Return(count_byte);
+}
