433f6c1ad23c8d5ea69902fccb526d8d7bbf191d
o0anais0o <choisy_anais@hotmail.fr>
2025-04-02 11:32:06 +0200

    Modification file 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,46 +3,32 @@
 #include "lists.h"
 
 /**
- * _strlen - Calcule la longueur d'une chaîne de caractères.
- * @s: Pointeur vers la chaîne à analyser.
- *
- * Return: Longueur de la chaîne.
- */
-unsigned int _strlen(const char *s)
-{
-unsigned int len = 0;
-
-while (s[len])
-len++;
-return (len);
-}
-
-/**
- * add_node - Adds a new node at the beginning of a list_t list.
- * @head: Double pointer to the head of the list.
- * @str: String to duplicate and add to the new node.
- *
- * Return: Address of the new element, or NULL if it failed.
- */
+* add_node - Adds a new node at the beginning of a list_t list.
+* @head: Double pointer to the head of the list.
+* @str: String to duplicate and add to the new node.
+*
+* Return: Address of the new element, or NULL if it failed.
+*/
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
+	list_t *new_node;
+	size_t length = 0;
 
-new_node = malloc(sizeof(list_t));
-if (!new_node) /*Allocate memory for new node*/
-return (NULL);
-
-new_node->str = strdup(str); /*Duplicate string and handle allocation failure*/
-if (!new_node->str)
-{
-free(new_node);
-return (NULL);
-}
-
-new_node->len = strlen(str); /* Calculate string length */
-new_node->next = *head; /*Insert at the end of the list*/
-
-*head = new_node;
-
-return (new_node); /*Return result*/
+	new_node = malloc(sizeof(list_t));
+	if (!new_node) /* Allocate memory for new node */
+		return (NULL);
+    /*Duplicate string and handle allocation failure*/
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	/* Calcul de la longueur manuellement */
+	while (str[length] != '\0')
+		length++;
+	new_node->len = length; /* Assignation de la longueur */
+	new_node->next = *head; /* Insert at the beginning of the list */
+	*head = new_node;
+	return (new_node); /* Return the new node */
 }
